fdf45f155fa08b0d2d444262be24cd62b83dc003
Artem Goncharov <artem.goncharov@gmail.com>
2025-03-10 13:39:46 +0100

    feat: Add SG and FIP resources

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # neutron-db-opa-proxy
+
+Small proxy to expose limited attributes of OpenStack Neutron resources
+necessary for the integration with the OpenPolicyAgent.
+
+Neutron implements custom oslo.policy checks requiring access to certain
+attributes of certain resources. It usually deals with them as a so-called
+OwnerCheck that i.e. verifies that parent resource of the resource that the
+user is currently trying to create/modify belongs to the same project (parent
+network of a subnet belongs to the same project). The check itself is inside of
+the Neutron code so when it is desired to delegate authorization decisions to
+the external system (Open Policy Agent) it is required to have access to the
+mentioned attributes. It is undesired to give access to the database directly
+(even in the read only mode). Neither is the OpenPolicyAgent by default capable
+of accessing DB. So this project comes as a tiny proxy exposing limited
+attributes through REST API allowing OPA accessing it also at the same time
+relying on the HTTP caching.
--- src/api/db.rs
+++ src/api/db.rs
@@ -8,36 +8,33 @@ use sea_orm::query::*;
 use crate::api::error::ApiError;
 use crate::api::types::*;
 use crate::db::{
-    networkrbacs as db_networkrbacs, networks as db_network,
-    prelude::{Networkrbacs as DbNetworkRbacs, Networks as DbNetwork},
+    floatingips as db_floating_ip, networkrbacs as db_networkrbacs, networks as db_network,
+    prelude::{
+        Floatingips as DbFloatingIp, Networkrbacs as DbNetworkRbacs, Networks as DbNetwork,
+        Securitygroups as DbSecurityGroups,
+    },
+    securitygroups as db_security_group,
 };
 
 #[async_trait]
 pub trait Neutron {
-    async fn get_network(
+    async fn get_floating_ip<'a>(
         &self,
         db: &DatabaseConnection,
-        id: String,
+        id: &'a str,
+    ) -> Result<Option<FloatingIP>, ApiError>;
+
+    async fn get_network<'a>(
+        &self,
+        db: &DatabaseConnection,
+        id: &'a str,
     ) -> Result<Option<Network>, ApiError>;
-    //        let select = DbNetwork::find_by_id(&id);
-    //        let entry: Option<db_network::Model> = select.one(db).await?;
-    //        match &entry {
-    //            Some(net) => {
-    //                let mut n = Network::from(net);
-    //                let rbac: Option<db_networkrbacs::Model> = DbNetworkRbacs::find()
-    //                    .filter(db_networkrbacs::Column::Action.eq("access_as_shared"))
-    //                    .filter(db_networkrbacs::Column::ObjectId.eq(&id))
-    //                    .filter(db_networkrbacs::Column::TargetProject.eq("*"))
-    //                    .one(db)
-    //                    .await?;
-    //                if rbac.is_some() {
-    //                    n.shared = true;
-    //                }
-    //                Ok(Some(n))
-    //            }
-    //            None => Ok(None),
-    //        }
-    //    }
+
+    async fn get_security_group<'a>(
+        &self,
+        db: &DatabaseConnection,
+        id: &'a str,
+    ) -> Result<Option<SecurityGroup>, ApiError>;
 }
 
 #[derive(Clone, Default)]
@@ -45,19 +42,29 @@ pub(crate) struct DbWorker {}
 
 #[async_trait]
 impl Neutron for DbWorker {
-    async fn get_network(
+    async fn get_floating_ip<'a>(
         &self,
         db: &DatabaseConnection,
-        id: String,
+        id: &'a str,
+    ) -> Result<Option<FloatingIP>, ApiError> {
+        let select = DbFloatingIp::find_by_id(id);
+        let entry: Option<db_floating_ip::Model> = select.one(db).await?;
+        Ok(entry.map(FloatingIP::from))
+    }
+
+    async fn get_network<'a>(
+        &self,
+        db: &DatabaseConnection,
+        id: &'a str,
     ) -> Result<Option<Network>, ApiError> {
-        let select = DbNetwork::find_by_id(&id);
+        let select = DbNetwork::find_by_id(id);
         let entry: Option<db_network::Model> = select.one(db).await?;
         match &entry {
             Some(net) => {
                 let mut n = Network::from(net);
                 let rbac: Option<db_networkrbacs::Model> = DbNetworkRbacs::find()
                     .filter(db_networkrbacs::Column::Action.eq("access_as_shared"))
-                    .filter(db_networkrbacs::Column::ObjectId.eq(&id))
+                    .filter(db_networkrbacs::Column::ObjectId.eq(id))
                     .filter(db_networkrbacs::Column::TargetProject.eq("*"))
                     .one(db)
                     .await?;
@@ -69,6 +76,16 @@ impl Neutron for DbWorker {
             None => Ok(None),
         }
     }
+
+    async fn get_security_group<'a>(
+        &self,
+        db: &DatabaseConnection,
+        id: &'a str,
+    ) -> Result<Option<SecurityGroup>, ApiError> {
+        let select = DbSecurityGroups::find_by_id(id);
+        let entry: Option<db_security_group::Model> = select.one(db).await?;
+        Ok(entry.map(SecurityGroup::from))
+    }
 }
 
 #[cfg(test)]
@@ -78,11 +95,25 @@ mock! {
 
     #[async_trait]
     impl Neutron for DbWorker {
-        async fn get_network(&self, db: &DatabaseConnection, id: String) -> Result<Option<Network>, ApiError>;
+        async fn get_floating_ip<'a>(&self, db: &DatabaseConnection, id: &'a str) -> Result<Option<FloatingIP>, ApiError>;
+        async fn get_network<'a>(&self, db: &DatabaseConnection, id: &'a str) -> Result<Option<Network>, ApiError>;
+        async fn get_security_group<'a>(&self, db: &DatabaseConnection, id: &'a str) -> Result<Option<SecurityGroup>, ApiError>;
     }
 
     impl Clone for DbWorker {
         fn clone(&self) -> Self;
+    }
+}
+
+impl From<db_floating_ip::Model> for FloatingIP {
+    fn from(value: db_floating_ip::Model) -> Self {
+        Self {
+            id: value.id.clone(),
+            floating_ip_address: value.floating_ip_address.clone(),
+            project_id: value.project_id.clone(),
+            tenant_id: value.project_id.clone(),
+            status: value.status.clone(),
+        }
     }
 }
 
@@ -99,12 +130,53 @@ impl From<&db_network::Model> for Network {
     }
 }
 
+impl From<db_security_group::Model> for SecurityGroup {
+    fn from(value: db_security_group::Model) -> Self {
+        Self {
+            id: value.id.clone(),
+            name: value.name.clone(),
+            project_id: value.project_id.clone(),
+            tenant_id: value.project_id.clone(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use sea_orm::{DatabaseBackend, MockDatabase, Transaction};
 
     use super::*;
-    use crate::db::{networkrbacs, networks};
+    use crate::db::{floatingips, networkrbacs, networks, securitygroups};
+
+    #[tokio::test]
+    async fn test_get_fip() {
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([vec![floatingips::Model {
+                id: "id".into(),
+                floating_ip_address: "1.2.3.4".into(),
+                floating_network_id: "fake".into(),
+                floating_port_id: "fake".into(),
+                fixed_port_id: None,
+                fixed_ip_address: None,
+                router_id: None,
+                last_known_router_id: None,
+                status: Some("broken".into()),
+                standard_attr_id: 0,
+                project_id: Some("project".into()),
+            }]])
+            .into_connection();
+
+        let _res = DbWorker {}.get_floating_ip(&db, "id").await;
+
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "floatingips"."project_id", "floatingips"."id", "floatingips"."floating_ip_address", "floatingips"."floating_network_id", "floatingips"."floating_port_id", "floatingips"."fixed_port_id", "floatingips"."fixed_ip_address", "floatingips"."router_id", "floatingips"."last_known_router_id", "floatingips"."status", "floatingips"."standard_attr_id" FROM "floatingips" WHERE "floatingips"."id" = $1 LIMIT $2"#,
+                ["id".into(), 1u64.into()]
+            ),]
+        );
+    }
 
     #[tokio::test]
     async fn test_get_network() {
@@ -127,7 +199,7 @@ mod tests {
             }]])
             .into_connection();
 
-        let _res = DbWorker {}.get_network(&db, "id".to_string()).await;
+        let _res = DbWorker {}.get_network(&db, "id").await;
 
         assert_eq!(
             db.into_transaction_log(),
@@ -148,6 +220,30 @@ mod tests {
                     ]
                 ),
             ]
+        );
+    }
+
+    #[tokio::test]
+    async fn test_get_sg() {
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results([vec![securitygroups::Model {
+                id: "id".into(),
+                name: Some("default".into()),
+                standard_attr_id: 0,
+                stateful: 0,
+                project_id: Some("project".into()),
+            }]])
+            .into_connection();
+
+        let _res = DbWorker {}.get_security_group(&db, "id").await;
+
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "securitygroups"."project_id", "securitygroups"."id", "securitygroups"."name", "securitygroups"."standard_attr_id", "securitygroups"."stateful" FROM "securitygroups" WHERE "securitygroups"."id" = $1 LIMIT $2"#,
+                ["id".into(), 1u64.into()]
+            ),]
         );
     }
 }
